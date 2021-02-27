2fd1d274adb87bfdfdbef888ba4f5b428a9843e7
ch_p <webserv2010@yandex.ru>
2021-02-27 22:11:26 +0300

    Add ship

---
+++ settings.py
@@ -0,0 +1,9 @@
+class Settings():
+    """Класс для хранения всех настроек игры Alien Invasion."""
+
+    def __init__(self):
+        """Инициализирует настройки игры."""
+        # Параметры экрана
+        self.screen_whidth = 1200
+        self.screen_height = 800
+        self.bg_color = (80, 80, 80)
